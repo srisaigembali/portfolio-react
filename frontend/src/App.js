3b19aa3d4285b2c2afcf53f1bc6434fcaf3efd43
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-07-29 11:32:31 +0530

    file and folder sturcture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonials,
+  Work,
+} from "./containers";
+import { Navbar } from "./components";
+import "./App.scss";
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
