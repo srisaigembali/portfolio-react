67476d882ea4bf01bd235a1f0eb82f8d787b537e
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-07-31 19:12:56 +0530

    integreted sanity with react

--- frontend/src/containers/About/About.jsx
+++ frontend/src/containers/About/About.jsx
@@ -1,37 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import images from "../../constants/images";
 import "./About.scss";
-
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am a good developer.",
-    imageUrl: images.about01,
-  },
-  {
-    title: "Frontend Development",
-    description: "I am a good developer.",
-    imageUrl: images.about02,
-  },
-  {
-    title: "Backend Development",
-    description: "I am a good developer.",
-    imageUrl: images.about03,
-  },
-  {
-    title: "MERN Stack",
-    description: "I am a good developer.",
-    imageUrl: images.about04,
-  },
-];
+import { urlFor, client } from "../../client";
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className='head-text'>
         I know that
-        <span> Good Design</span>
+        <span> Good Development</span>
         <br />
         means
         <span> Good business</span>
@@ -46,7 +32,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imageUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>
               {about.title}
             </h2>
