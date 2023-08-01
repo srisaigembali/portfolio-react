ec8e86d2a4fc24651a7ace23415d91de62d308fe
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-08-01 14:19:32 +0530

    added contact form

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -16,12 +16,14 @@ const Navbar = () => {
         </h1>
       </div>
       <ul className='app__navbar-links'>
-        {["home", "about", "work", "skills", "contact"].map((item) => (
-          <li className='app__flex p-text' key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
+        {["home", "about", "work", "skills", "testimonials", "contact"].map(
+          (item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        )}
       </ul>
 
       <div className='app__navbar-menu'>
@@ -34,7 +36,14 @@ const Navbar = () => {
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {["home", "about", "work", "skills", "contact"].map((item) => (
+              {[
+                "home",
+                "about",
+                "work",
+                "skills",
+                "testimonials",
+                "contact",
+              ].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
--- frontend/src/containers/Testimonials/Testimonials.jsx
+++ frontend/src/containers/Testimonials/Testimonials.jsx
@@ -94,6 +94,6 @@ const Testimonial = () => {
 
 export default AppWrap(
   MotionWrap(Testimonial, "app__testimonial"),
-  "testimonial",
+  "testimonials",
   "app__primarybg"
 );
