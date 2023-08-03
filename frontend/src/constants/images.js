27b62f7b65a205b720f1cda1eb24317c4b4af00c
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-08-03 08:54:27 +0530

    profile image updated

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -26,6 +26,7 @@ import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
 
 import profile from "../assets/profile.png";
+import profile2 from "../assets/profile2.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
 
@@ -64,6 +65,7 @@ const images = {
   about03,
   about04,
   profile,
+  profile2,
   circle,
   logo,
   adidas,
